791acf04f1af40c032f9aaa0ad05f18a826fdd31
rafajr91 <rah5024@gmail.com>
2017-10-24 15:01:57 -0700

    Created function that fetches images from Mongo and renders them to page. #1

--- public/main.js
+++ public/main.js
@@ -2,9 +2,21 @@ const $slideshow = document.querySelector('.slideshow')
 
 const renderImg = (image) => {
   const $slideImg = document.createElement('img')
+  $slideImg.setAttribute('class', 'slide-img')
   $slideImg.setAttribute('src', image.imgSrc)
   $slideImg.setAttribute('alt', image.altImgName)
 
-  $slideshow.appendChild($slideImg)
-  return $slideshow
+  return $slideImg
 }
+
+const fetchImgs = () => {
+  fetch('http://localhost:3000/slides')
+    .then(res => res.json())
+    .then(images => {
+      images
+        .map(renderImg)
+        .forEach($image => $slideshow.appendChild($image))
+    })
+}
+
+fetchImgs()
