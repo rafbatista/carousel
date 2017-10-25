eed2107c925d5409da211048701bce672d50c367
rafajr91 <rah5024@gmail.com>
2017-10-25 09:28:55 -0700

    Refactored front-end functions to generate images on a loop. #1

--- public/main.js
+++ public/main.js
@@ -1,22 +1,28 @@
-const $slideshow = document.querySelector('.slideshow')
-
-const renderImg = (image) => {
-  const $slideImg = document.createElement('img')
-  $slideImg.setAttribute('class', 'slide-img')
-  $slideImg.setAttribute('src', image.imgSrc)
-  $slideImg.setAttribute('alt', image.altImgName)
-
-  return $slideImg
-}
-
-const fetchImgs = () => {
-  fetch('http://localhost:3000/slides')
+const slides = () => {
+  return fetch('/slides')
     .then(res => res.json())
-    .then(images => {
-      images
-        .map(renderImg)
-        .forEach($image => $slideshow.appendChild($image))
-    })
 }
 
-fetchImgs()
+const carousel = {
+  slides: null,
+  current: 0
+}
+
+slides()
+  .then((images) => {
+    carousel.slides = images
+    console.log(carousel.slides)
+  })
+
+setInterval(() => {
+  document
+    .querySelector('.slide')
+    .setAttribute('src', `${carousel.slides[carousel.current].imgSrc}`)
+
+  if (carousel.current < carousel.slides.length - 1) {
+    carousel.current++
+  }
+  else {
+    carousel.current = 0
+  }
+}, 3000)
