bce79538f3a0ad0b09c2f000e6ed8756ed9d78e9
rafajr91 <rah5024@gmail.com>
2017-10-25 13:15:25 -0700

    Added arrows and selectors to page via index.html. Gave the arrows click functionality. #1

--- public/main.js
+++ public/main.js
@@ -11,18 +11,36 @@ const carousel = {
 slides()
   .then((images) => {
     carousel.slides = images
-    console.log(carousel.slides)
   })
+  .then(() => changeSlide())
 
-setInterval(() => {
+const changeSlide = () => {
   document
     .querySelector('.slide')
     .setAttribute('src', `${carousel.slides[carousel.current].imgSrc}`)
+}
 
+setInterval(() => {
   if (carousel.current < carousel.slides.length - 1) {
     carousel.current++
   }
   else {
     carousel.current = 0
   }
+  changeSlide()
 }, 3000)
+
+const $prevButton = document.querySelector('.fa-arrow-left')
+
+$prevButton.addEventListener('click', () => {
+  if (carousel.current === 0) return
+  carousel.current--
+  changeSlide()
+})
+
+const $nextButton = document.querySelector('.fa-arrow-right')
+$nextButton.addEventListener('click', () => {
+  if (carousel.current === 2) return
+  carousel.current++
+  changeSlide()
+})
